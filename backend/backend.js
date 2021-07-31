5e1438720bb6f0a22af6827c60f8025e2ea6293b
Raudo José Céspedes <raj.cespedes@gmail.com>
2021-07-30 20:32:37 -0400

    Unchecking the DB transaction

--- backend/backend.js
+++ backend/backend.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
 
-mongoose.connect('mongodb://localhost:27017/cv', {useNewUrlParser: true} );
+// mongoose.connect('mongodb://localhost:27017/cv', {useNewUrlParser: true} );
 
 const connection = mongoose.connection;
 
@@ -23,23 +23,23 @@ connection.once('open', () => console.log('Connected to Database'));
 
 app.get('/', (req,res) => res.send('backend page') );
 
-app.post('/', function(req,res) {
+// app.post('/', function(req,res) {
 
-    Demografico.create({
-        nombres: req.body.nombres,
-        apellidos: req.body.apellidos,
-        ocupacion: req.body.ocupacion,
-        barrio: req.body.barrio,
-        celular: req.body.celular,
-        telefono: req.body.telefono,
-        email: req.body.email,
-        licencia: req.body.licencia,
-        vehiculo: req.body.vehiculo,
-        traslado: req.body.traslado
-    }, (err,passed) => !err ? console.log('Data added') : console.log(err));
+//     Demografico.create({
+//         nombres: req.body.nombres,
+//         apellidos: req.body.apellidos,
+//         ocupacion: req.body.ocupacion,
+//         barrio: req.body.barrio,
+//         celular: req.body.celular,
+//         telefono: req.body.telefono,
+//         email: req.body.email,
+//         licencia: req.body.licencia,
+//         vehiculo: req.body.vehiculo,
+//         traslado: req.body.traslado
+//     }, (err,passed) => !err ? console.log('Data added') : console.log(err));
     
 
-} );
+// } );
 
 
  
